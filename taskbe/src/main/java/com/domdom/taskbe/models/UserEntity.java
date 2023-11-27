922a18d2f7e8359caefa6fc0bcd0122257db06ac
ducduc222 <ducduc28052002@gmail.com>
2023-11-27 13:17:40 +0700

    upcode

---
+++ taskbe/src/main/java/com/domdom/taskbe/models/UserEntity.java
@@ -0,0 +1,46 @@
+package com.domdom.taskbe.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.domdom.taskbe.models.enums.*;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Date;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
+public class UserEntity extends BaseEntity {
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(name = "is_active", columnDefinition = "boolean default false")
+    private boolean isActive;
+
+    private String password;
+    @Column(unique = true)
+    private String email;
+    private String address;
+
+    private Date dob;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    @JsonManagedReference
+    private List<Role> roles;
+}
